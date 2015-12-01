c56ba1b2fcf35e4b54604b68e3e25c584b6bbb2c
Silvia Brown <silscript@gmail.com>
2015-11-30 19:35:05 -0500

    Added task to compiles sass.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,19 @@
 var gulp = require('gulp');
 
 // Require Plugins
-gulp.task('default', function() {
-  //
+// var browserSync = require('browser-sync');
+// var concat = require('gulp-concat');
+// var jshint = require('gulp-jshint');
+// var rename = require('gulp-rename');
+// var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
+
+// Compile Sass
+gulp.task('sass', function() {
+  return gulp.src('scss/main.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('assets/css'));
 });
+
+// Default Task
+gulp.task('default', ['sass']);
